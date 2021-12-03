2252c3bda14be92d7c41408588dcb7a0fcaac8c2
Matt DiPerna <mdiperna216@gmail.com>
2021-12-03 14:21:01 -0500

    Initial Commit

---
+++ Scripting Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Scripting Project
+//
+//  Created by Matthew DiPerna on 12/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
